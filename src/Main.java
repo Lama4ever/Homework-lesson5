e63500a119caa78f7eec3dbd7b9d7ee545536b48
Lama4ever <pychav@gmail.com>
2023-10-19 20:50:09 +0200

    updated exceptions

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,46 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
 
         PlantList list = new PlantList();
-        list.loadPlantList(Settings.getPATH()+Settings.getFilenameCorrect());
-        list.addPlant(new Plant("Bazalka v kuchyni","", LocalDate.parse("2021-09-04"),
-                LocalDate.parse("2021-09-04"), 3 ));
-        list.removePlant(2);
-        list.savePlantList(Settings.getPATH()+Settings.getFilenameOut());
+        try {
+            list.loadPlantList(Settings.getPATH() + Settings.getFilenameCorrect());
+        } catch (PlantException e) {
+            System.err.println(e.getLocalizedMessage());
+        }
+        try {
+            list.addPlant(new Plant("Bazalka v kuchyni", "", LocalDate.parse("2021-09-04"),
+                    LocalDate.parse("2021-09-04"), 3));
+        } catch (PlantException e) {
+            System.err.println(e.getLocalizedMessage());
+        }
 
-        list.loadPlantList(Settings.getPATH()+Settings.getFilenameWrongFrequency());
+        try {
+            list.removePlant(2);
+        } catch (PlantException e) {
+            System.err.println(e.getLocalizedMessage());
+        }
 
-        list.loadPlantList(Settings.getPATH()+Settings.getFilenameWrongDate());
+        try {
+            list.savePlantList(Settings.getPATH() + Settings.getFilenameOut());
+        } catch (IOException e) {
+            System.err.println("Chyba v souboru:" + Settings.getPATH() + Settings.getFilenameOut() + " " + e.getMessage());
+        }
+        try {
+            list.loadPlantList(Settings.getPATH() + Settings.getFilenameWrongFrequency());
+        } catch (PlantException e) {
+            System.err.println("Chyba v souboru:" + Settings.getPATH() + Settings.getFilenameWrongFrequency() + " " + e.getLocalizedMessage());
+        }
+        try {
+            list.loadPlantList(Settings.getPATH() + Settings.getFilenameWrongDate());
+        } catch (PlantException e) {
+            System.err.println("Chyba v souboru:" + Settings.getPATH() + Settings.getFilenameWrongDate() + " " + e.getLocalizedMessage());
+        }
 
     }
+
 }
--- src/PlantList.java
+++ src/PlantList.java
@@ -11,46 +11,46 @@ public class PlantList {
         this.plantList = new ArrayList<>();
     }
 
-    public void addPlant(Plant plant){
+    public void addPlant(Plant plant) {
         plantList.add(plant);
     }
-    public void removePlant(int index){
+
+    public void removePlant(int index) throws PlantException {
+        if (index > plantList.size() - 1) throw new PlantException("Index k odstraneni neexistuje");
         plantList.remove(index);
     }
 
-    public Plant getPlant(int index){
+    public Plant getPlant(int index) {
         return plantList.get(index);
     }
 
-    public void loadPlantList(String path)
-    {
-        try (Scanner s = new Scanner(new BufferedReader(new FileReader(path)));) {
+    public void loadPlantList(String path) throws PlantException {
+
+        try (Scanner s = new Scanner(new BufferedReader(new FileReader(path)))) {
             while (s.hasNextLine()) {
                 plantList.add(Plant.parsePlant(s.nextLine()));
             }
-        } catch (NumberFormatException | IOException | PlantException ex ) {
-            System.err.println("Chyba při čtení souboru " + path + ": "
-                    + ex.getLocalizedMessage());
+        } catch (FileNotFoundException e) {
+            throw new PlantException("Nepodarilo se najit soubor: " + path);
+        } catch (PlantException e) {
+            throw new PlantException(e.getLocalizedMessage());
         }
+
     }
 
-    public void savePlantList(String path)
-    {
+    public void savePlantList(String path) throws IOException {
         try (PrintWriter outputWriter =
-                     new PrintWriter(new BufferedWriter(new FileWriter(path)))) {
+                     new PrintWriter(new BufferedWriter(new FileWriter(path)));) {
             for (Plant plant : plantList) {
-                outputWriter.println(plant.toString());
+                outputWriter.println(plant);
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 
     @Override
     public String toString() {
         StringBuilder reply = new StringBuilder();
-        for(Plant plant: plantList)
-        {
+        for (Plant plant : plantList) {
             reply.append(plant.toString()).append("\n");
         }
         return reply.toString();
